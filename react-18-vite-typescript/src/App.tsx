a6d66eefc0fc5cbf2265b8eb2691549a57d4ffcc
Matthew Foyle <mfoyle93@gmail.com>
2024-03-16 12:48:42 +0000

    fix: hook

--- react-18-vite-typescript/src/App.tsx
+++ react-18-vite-typescript/src/App.tsx
@@ -1,20 +1,16 @@
-import React from 'react';
-import { ethers } from 'ethers';
-import CounterABI from '../../foundry/out/Counter.sol/Counter.json'; // Adjust the path to your ABI
-const counterAddress = '0xd1f65f859609AFB5feC237b98Cd90f918bbb7dB9'; // Replace with your contract's address
+import React from "react";
+import { ethers } from "ethers";
+import CounterABI from "../../foundry/out/Counter.sol/Counter.json"; // Adjust the path to your ABI
+const counterAddress = "0xd1f65f859609AFB5feC237b98Cd90f918bbb7dB9"; // Replace with your contract's address
 
 import { useEffect, useState } from "react";
 import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
 import { createWalletClient, custom, type WalletClient } from "viem";
-import {
-  DynamicWidget,
-} from "@dynamic-labs/sdk-react-core";
+import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
 
 import "./App.css";
 
-
 function App() {
-
   // const [signer, setSigner] = useState(null);
   // const { primaryWallet } = useDynamicContext();
 
@@ -42,33 +38,44 @@ function App() {
 
   // console.log("signer", signer);
 
-  const [currentNumber, setCurrentNumber] = React.useState('Fetching...');
+  const [currentNumber, setCurrentNumber] = React.useState("Fetching...");
 
+  const { primaryWallet } = useDynamicContext();
 
   const fetchNumber = async () => {
-    const { primaryWallet } = useDynamicContext();
+    if (!primaryWallet) return;
+
     const signer = await primaryWallet.connector.ethers?.getSigner();
     console.log("signer", signer);
-    const counterContract = new ethers.Contract(counterAddress, CounterABI, signer);
+    const counterContract = new ethers.Contract(
+      counterAddress,
+      CounterABI,
+      signer
+    );
     try {
       const number = await counterContract.getNumber();
       setCurrentNumber(number.toString());
     } catch (error) {
-      console.error('Error fetching number:', error);
-      setCurrentNumber('Error fetching number');
+      console.error("Error fetching number:", error);
+      setCurrentNumber("Error fetching number");
     }
   };
 
   useEffect(() => {
     fetchNumber();
-  }, []);
+  }, [primaryWallet]);
 
   return (
     <>
       <DynamicWidget />
-      <div>Current Number: {currentNumber}</div>
-      <button onClick={fetchNumber}>Refresh Number</button>
-      {/* <Test /> */}
+      {primaryWallet ? (
+        <div>
+          <div>Current Number: {currentNumber}</div>
+          <button onClick={fetchNumber}>Refresh Number</button>
+        </div>
+      ) : (
+        <div>Connect your wallet to see the current number</div>
+      )}
     </>
   );
 }
